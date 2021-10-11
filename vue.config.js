fea501cf77923bfc5145e4c67319b80abdd83e75
Sergey Chydym <chaaaccaunt@gmail.com>
2021-10-11 22:58:00 +0700

    Forgot to change title

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,10 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Портфолио Чыдым С. Б.";
+      return args;
+    });
+  },
 };
